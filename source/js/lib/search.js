b32c987950c0477ded6a547783094ced76d5b04d
skybase <1054897561@qq.com>
2026-03-08 23:28:03 +0800

    1添加搜索栏 2添加markdown的目录 3稍微丰富了一下简介栏

--- source/js/lib/search.js
+++ source/js/lib/search.js
@@ -4,16 +4,33 @@ mixins.search = {
     },
     watch: {
         search(value) {
-            let timeline = this.$refs.timeline.childNodes;
-            for (let i of timeline)
+            // archives page
+            if (this.$refs.timeline) {
+                let timeline = this.$refs.timeline.childNodes;
+                for (let i of timeline)
+                    if (!value || i.dataset.title.includes(value)) {
+                        i.style.opacity = 1;
+                        i.style.visibility = "visible";
+                        i.style.marginTop = 0;
+                    } else {
+                        i.style.opacity = 0;
+                        i.style.visibility = "hidden";
+                        i.style.marginTop = -i.offsetHeight - 30 + "px";
+                    }
+            }
+            // home page
+            let posts = document.querySelectorAll(".post[data-title]");
+            for (let i of posts)
                 if (!value || i.dataset.title.includes(value)) {
                     i.style.opacity = 1;
                     i.style.visibility = "visible";
-                    i.style.marginTop = 0;
+                    i.style.height = "";
+                    i.style.marginBottom = "";
                 } else {
                     i.style.opacity = 0;
                     i.style.visibility = "hidden";
-                    i.style.marginTop = -i.offsetHeight - 30 + "px";
+                    i.style.height = 0;
+                    i.style.marginBottom = 0;
                 }
         },
     },
